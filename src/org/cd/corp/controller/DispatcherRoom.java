c1cca38c90098956165bd19f7aa058713fa0de07
CyberDoge <tagirman998@gmail.com>
2018-07-24 22:03:51 +0400

    simple architecture

--- src/org/cd/corp/controller/DispatcherRoom.java
+++ src/org/cd/corp/controller/DispatcherRoom.java
@@ -1,4 +1,47 @@
 package org.cd.corp.controller;
 
+import org.cd.corp.model.Coordinates;
+import org.cd.corp.model.DistributionCenter;
+import org.cd.corp.model.Order;
+import org.cd.corp.model.TimeWindow;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DispatcherRoom {
+    private DistributionCenter distributionCenter;
+    private List<Order> orderListToNextDay;
+    private List<Order> optimizedOrderList;
+
+    public DispatcherRoom(double dispatcherLatitude, double dispatcherLongitude, int startMin, int startHour,
+                          int endMin, int endHour, int fleet) {
+        //coordinates
+        var distributionCenterCoordinates = new Coordinates(dispatcherLatitude, dispatcherLongitude);
+        //startTime
+        var distributionStartTime = LocalTime.of(startHour, startMin);
+        //endTime
+        var distributionEndTime = LocalTime.of(endHour, endMin);
+        //timeWindow
+        var distributionTimeWindow = new TimeWindow(distributionStartTime, distributionEndTime);
+
+        distributionCenter = new DistributionCenter(distributionCenterCoordinates, distributionTimeWindow, fleet);
+        orderListToNextDay = new ArrayList<>();
+    }
+
+    public void optimizeOrders() {
+        var sorted = orderListToNextDay.stream().sorted(Comparator.comparing((order) ->
+                order.getTimeWindow().getStartingTime())).collect(Collectors.toList());
+        optimizedOrderList = new ArrayList<>();
+        distributionCenter.freeResource();
+
+    }
+
+    public void addOrder(Order order) {
+        assert order != null;
+        orderListToNextDay.add(order);
+    }
+
 }
--- src/org/cd/corp/model/Order.java
+++ src/org/cd/corp/model/Order.java
@@ -8,4 +8,32 @@ public class Order {
     private TimeWindow timeWindow;
     private LocalTime uploadTime;
     private LocalTime unloadTime;
+
+    public Order(Coordinates coordinates, int weight, TimeWindow timeWindow, LocalTime uploadTime, LocalTime unloadTime) {
+        this.coordinates = coordinates;
+        this.weight = weight;
+        this.timeWindow = timeWindow;
+        this.uploadTime = uploadTime;
+        this.unloadTime = unloadTime;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public TimeWindow getTimeWindow() {
+        return timeWindow;
+    }
+
+    public LocalTime getUploadTime() {
+        return uploadTime;
+    }
+
+    public LocalTime getUnloadTime() {
+        return unloadTime;
+    }
 }
